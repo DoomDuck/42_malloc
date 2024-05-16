e11b15db17c2824b2b535fffc9e9eeefa343378d
DoomDuck <docdoomduck@protonmail.com>
2024-05-16 11:33:47 +0200

    Chunk & Page structure

---
+++ include/internals.h
@@ -0,0 +1,40 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/*
+ * Chunk
+ */
+typedef struct {
+	size_t size_divided_by_16 : sizeof(size_t) - 3;
+	bool arena: 1;
+	bool mmaped: 1;
+	bool previous_in_use: 1;
+} chunk_header;
+
+typedef union {
+	uint8_t payload;
+	struct {
+		struct s_chunk* previous;
+		struct s_chunk* next;
+	} list;
+} __attribute__((aligned(16))) chunk_body;
+
+typedef struct s_chunk {
+	size_t previous_chunk_size;
+	chunk_header header;
+	chunk_body body;
+} chunk;
+
+void chunk_init_alloc(chunk* self, size_t previous_chunk_size, size_t size);
+chunk* chunk_of_payload(void* payload);
+size_t chunk_size(chunk* self);
+
+/*
+ * Page
+ */
+typedef struct s_page {
+	size_t size;
+	struct s_page* next;
+	chunk first_chunk;
+} page;
--- src/lib.c
+++ src/lib.c
@@ -1,4 +1,5 @@
 #include <malloc.h>
+#include <internals.h>
 
 void free(void* ptr) {
 	(void)ptr;
@@ -13,4 +14,20 @@ void* realloc(void* ptr, size_t size) {
 	(void)ptr;
 	(void)size;
 	return NULL;
+}
+
+void chunk_init_alloc(chunk* self, size_t previous_chunk_size, size_t size) {
+	self->previous_chunk_size = previous_chunk_size;
+	// TODO: assert that: size >= sizeof(chunk)
+	// TODO: check size is a multiple of 16
+	self->header.size_divided_by_16 = size / 16;
+}
+
+size_t chunk_size(chunk* self) {
+	return self->header.size_divided_by_16 * 16;
+}
+
+chunk* chunk_of_payload(void *payload) {
+	size_t offset = (size_t)(&((chunk*)NULL)->body.payload);
+	return (chunk*)((uint8_t*)payload - offset);
 }
